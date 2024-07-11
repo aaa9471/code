a97741a87ddfad15f5dbde993f463e55b982aa88
jackson <aaa9471@gmail.com>
2024-07-11 22:23:23 +0800

    first commit

---
+++ hello world/hello world/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  hello world
+//
+//  Created by Yeh, Hsuan-Wei on 2024/7/5.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    
+    int w;
+    cin>>w;
+    
+    if(w > 2 && w % 2 == 0)cout<<"YES";
+    else cout<<"NO";
+    
+    return 0;
+}
